7c59030105d74fdb8364a6891033c603985d0d5c
Gaston Gallegos <gaston52343@gmail.com>
2025-09-28 12:09:03 -0300

    refactor and enhance review functions; add custom list processing functions

--- guide-4.hs
+++ guide-4.hs
@@ -1,3 +1,44 @@
 {-# LANGUAGE NPlusKPatterns #-}
 
-data Arbol = Hoja | Nodo Int Arbol
+data Color = Rojo | Naranja | Amarillo | Verde | Azul | Violeta
+  deriving (Show)
+
+-- data Arbol = Hoja | Nodo Int Arbol
+
+data ArbolGenerico a = Hoja | Nodo a (ArbolGenerico a) (ArbolGenerico a)
+  deriving (Show)
+
+arbol3 :: ArbolGenerico Int
+arbol3 =
+  Nodo
+    4
+    ( Nodo
+        2
+        (Nodo 1 Hoja Hoja)
+        (Nodo 3 Hoja Hoja)
+    )
+    ( Nodo
+        7
+        ( Nodo
+            5
+            Hoja
+            (Nodo 6 Hoja Hoja)
+        )
+        (Nodo 8 Hoja Hoja)
+    )
+
+preorder :: ArbolGenerico a -> [a]
+preorder Hoja = []
+preorder (Nodo x izq der) = [x] ++ preorder izq ++ preorder der
+
+inorder :: ArbolGenerico a -> [a]
+inorder Hoja = []
+inorder (Nodo x izq der) = inorder izq ++ [x] ++ inorder der
+
+postorder :: ArbolGenerico a -> [a]
+postorder Hoja = []
+postorder (Nodo x izq der) = postorder izq ++ postorder der ++ [x]
+
+reflejar :: ArbolGenerico a -> ArbolGenerico a
+reflejar Hoja = Hoja
+reflejar (Nodo x izq der) = Nodo x (reflejar der) (reflejar izq)
--- review.hs
+++ review.hs
@@ -28,63 +28,90 @@ multiplica :: Int -> [Int] -> [Int]
 multiplica z [] = []
 multiplica z (x : xs) = z * x : multiplica z xs
 
-
---acummulators
+-- acummulators
 
 prodCuadrados :: [Int] -> Int
 prodCuadrados [] = 1
-prodCuadrados (x:xs) = (x*x) * prodCuadrados xs
+prodCuadrados (x : xs) = (x * x) * prodCuadrados xs
 
 todosMenores10 :: [Int] -> Bool
 todosMenores10 [] = True
-todosMenores10 (x:xs) | x>=10 = False
-                      | otherwise = todosMenores10 xs
-
+todosMenores10 (x : xs)
+  | x >= 10 = False
+  | otherwise = todosMenores10 xs
 
 quitar0s :: [Int] -> [Int]
 quitar0s [] = []
-quitar0s (x:xs)
+quitar0s (x : xs)
   | x == 0 = quitar0s xs
-  | otherwise = x: quitar0s xs
-
+  | otherwise = x : quitar0s xs
 
 maximo :: [Int] -> Int
 maximo [x] = x
-maximo (x:xs)
+maximo (x : xs)
   | x > maximo xs = x
   | otherwise = maximo xs
 
-
 todos0y1 :: [Int] -> Bool
 todos0y1 [] = True
-todos0y1 (x:xs)
+todos0y1 (x : xs)
   | x /= 0 && x /= 1 = False
-  | otherwise = todos0y1 xs 
+  | otherwise = todos0y1 xs
 
 ultimo :: [a] -> a
 ultimo [x] = x
-ultimo (_:xs) = ultimo xs
+ultimo (_ : xs) = ultimo xs
 
 rev :: [a] -> [a]
 rev [] = []
-rev (x:xs) = rev xs ++ [x]
+rev (x : xs) = rev xs ++ [x]
 
---zip fn
-repartir :: [String] -> [String] -> [(String,String)]
-repartir [][] = []
-repartir (x:xs) (y:ys) = (x,y) : repartir xs ys
+-- zip fn
+repartir :: [String] -> [String] -> [(String, String)]
+repartir [] [] = []
+repartir (x : xs) (y : ys) = (x, y) : repartir xs ys
 
---unzip
-apellidos :: [(String,String,Int)] -> [String]
+-- unzip
+apellidos :: [(String, String, Int)] -> [String]
 apellidos [] = []
-apellidos ((nombre,apellido,edad):xs) = apellido : apellidos xs 
+apellidos ((nombre, apellido, edad) : xs) = apellido : apellidos xs
 
---custom
+-- custom
 
 myLength :: [a] -> Int
 myLength [] = 0
-myLength (x:xs) = 1 + myLength xs 
+myLength (x : xs) = 1 + myLength xs
 
 myIndex :: [a] -> Int -> a
-myIndex [x] 0 = x
-myIndex (_:xs) inx = myIndex xs (inx-1)  
+myIndex [] _ = error "Index too big"
+myIndex (x : _) 0 = x
+myIndex (_ : xs) inx = myIndex xs (inx - 1)
+
+myHead :: [a] -> a
+myHead [] = error "empty list"
+myHead (x : _) = x
+
+myTail :: [a] -> a
+myTail [] = error "empty list"
+myTail [x] = x
+myTail (_ : xs) = myTail xs
+
+myTake :: Int -> [a] -> [a]
+myTake _ [] = []
+myTake n _ | n <= 0 = []
+myTake n (x : xs) = x : myTake (n - 1) xs
+
+myDrop :: [a] -> Int -> [a]
+myDrop [] _ = []
+myDrop arr 0 = arr
+myDrop (x : xs) n
+  | n > 0 = myDrop xs (n - 1)
+  | otherwise = xs
+
+myConcat :: [a] -> [a] -> [a]
+myConcat [] arr = arr
+myConcat (x : xs) arr = x : myConcat xs arr
+
+(<+:) :: [a] -> a -> [a]
+[] <+: y = [y]
+(x : xs) <+: y = x : (xs <+: y)
